378131e7e118f84a0169638a90bdcba650b72936
Torsten Martinsen <torsten@bullestock.net>
2017-06-26 20:49:54 +0200

    Add Python test script.

---
+++ python/test.py
@@ -0,0 +1,31 @@
+import socket, time
+
+# addressing information of target
+IPADDR = '192.168.0.58'
+#IPADDR = '10.42.2.246'
+PORTNUM = 7890
+
+# initialize a socket, think of it as a cable
+# SOCK_DGRAM specifies that this is UDP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+ 
+# connect the socket, think of it as connecting the cable to the address location
+s.connect((IPADDR, PORTNUM))
+
+d = 0.2
+
+while True:
+    data = '570400000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff00'
+    
+    s.send(data.decode('hex'))
+
+    time.sleep(d)
+    
+    data = '57040000ff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ffff000000ff000000ff'
+    
+    s.send(data.decode('hex'))
+
+    time.sleep(d)
+    
+# close the socket
+s.close()
