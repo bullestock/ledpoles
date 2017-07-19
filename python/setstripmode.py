75d24e5e7fc4a65159881fc0e6ffe8c9b352d465
Torsten Martinsen <torsten@bullestock.net>
2017-07-19 14:04:55 +0200

    Set strip mode.

---
+++ python/setstripmode.py
@@ -0,0 +1,22 @@
+import socket, time, sys
+
+if len(sys.argv) != 2:
+    print("Usage: setstripmode <mode>")
+    sys.exit()
+
+mode = int(sys.argv[1])
+print("Setting strip mode %d" % mode)
+# addressing information of target
+IPADDR = '192.168.0.58'
+PORTNUM = 7890
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+s.connect((IPADDR, PORTNUM))
+
+d = 0.2
+
+data = '5805%02x' % mode
+    
+s.send(data.decode('hex'))
+
+s.close()
