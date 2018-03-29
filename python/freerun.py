b672cc7cbb118be53f4566ec5eb09630e267eaf9
Torsten Martinsen <torsten@bullestock.net>
2018-03-30 00:56:32 +0200

    Set freerun mode.

---
+++ python/freerun.py
@@ -0,0 +1,23 @@
+import socket, time, sys
+
+if len(sys.argv) < 2:
+    print("Usage: freerun [<address>]")
+    sys.exit()
+
+address = 'displaydingo1.local'
+
+if len(sys.argv) > 1:
+    address = sys.argv[1]
+    
+print("Setting free run mode for %s" % address)
+
+PORTNUM = 7890
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+s.connect((address, PORTNUM))
+
+data = '5B04'
+    
+s.send(data.decode('hex'))
+
+s.close()
